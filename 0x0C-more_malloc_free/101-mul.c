8faa49ca1e1bd88333bcda3968ea7264fce1ff5d
Oyindamola Dawodu <kudiratoyindamolad@gmail.com>
2022-05-09 19:44:21 +0000

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -25,7 +25,7 @@ int find_len(char *str)
 }
 /**
  * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
+ *                  the character 'x'. Adds a terminating null byte.
  * @size: The size of the array to be initialized.
  *
  * Description: If there is insufficient space, the
